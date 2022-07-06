3565d3ed437021e18d466016f15114f0cf7958f8
Kwakdeung <rhkrwnsdud22@naver.com>
2022-07-06 15:29:28 +0900

    ex04_03_add_a_stateful_widget

---
+++ Get_started/ex04_03_add_a_stateful_widget/android/app/src/main/kotlin/com/example/ex04_03_add_a_stateful_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex04_03_add_a_stateful_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Get_started/ex04_03_add_a_stateful_widget/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RandomWords());
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
