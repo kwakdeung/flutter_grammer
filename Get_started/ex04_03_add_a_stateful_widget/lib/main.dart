8327831627c343caf2d742156d459c753b808eda
Kwakdeung <rhkrwnsdud22@naver.com>
2022-07-06 16:23:09 +0900

    ex04_03_add_a_stateful_widget

--- Get_started/ex04_03_add_a_stateful_widget/lib/main.dart
+++ Get_started/ex04_03_add_a_stateful_widget/lib/main.dart
@@ -1,17 +1,13 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const RandomWords());
+  runApp(const MyApp());
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,5 +21,19 @@ class _RandomWordsState extends State<RandomWords> {
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
