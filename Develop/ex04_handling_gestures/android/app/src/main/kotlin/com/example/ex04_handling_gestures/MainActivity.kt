0b0db83448639db3ffe0514cfbd76caca47bb442
kwakdeung <51012589a@gmail.com>
2022-07-08 11:11:16 +0900

    ex04_handling_gestures

---
+++ Develop/ex04_handling_gestures/android/app/src/main/kotlin/com/example/ex04_handling_gestures/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex04_handling_gestures
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Develop/ex04_handling_gestures/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // GestureDetector: 입력 제스처를 감지하는 것
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0), // 안쪽 영역
+        margin: const EdgeInsets.symmetric(horizontal: 8.0), // 바깥쪽 영역
+        // symmetric: 대칭(출처: Google 사전)
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
+      ),
+    ),
+  );
+}
