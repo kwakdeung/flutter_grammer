f7ffb003ed3801d745fb6dbbaf65e16d5a2b8294
kwakdeung <51012589a@gmail.com>
2022-07-15 11:56:14 +0900

    ex10_02_simplifying_with_animatedwidget

---
+++ Develop/ex10_02_simplifying_with_animatedwidget/android/app/src/main/kotlin/com/example/ex10_02_simplifying_with_animatedwidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex10_02_simplifying_with_animatedwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Develop/ex10_02_simplifying_with_animatedwidget/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const LogoApp());
+
+class LogoApp extends StatefulWidget {
+  const LogoApp({super.key});
+
+  @override
+  State<LogoApp> createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: const FlutterLogo(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // dispose: State 객체를 영구적으로 없앨 때 호출
+    controller.dispose();
+    super.dispose();
+  }
+}
