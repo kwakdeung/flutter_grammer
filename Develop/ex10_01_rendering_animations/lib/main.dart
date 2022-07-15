2c12cf382b453a95d575a0ab897cc3a5ed8babfe
kwakdeung <51012589a@gmail.com>
2022-07-15 11:41:06 +0900

    ex10_01_rendering_animations

--- Develop/ex10_01_rendering_animations/lib/main.dart
+++ Develop/ex10_01_rendering_animations/lib/main.dart
@@ -9,16 +9,40 @@ class LogoApp extends StatefulWidget {
   State<LogoApp> createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
+        height: animation.value,
+        width: animation.value,
         child: const FlutterLogo(),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // dispose: State 객체를 영구적으로 없앨 때 호출
+    controller.dispose();
+    super.dispose();
   }
 }
