094d8cacba7ff0db4af5ed59c5c60b8633b83921
kwakdeung <51012589a@gmail.com>
2022-07-15 13:29:51 +0900

    ex10_05_simultaneous_animations

---
+++ Develop/ex10_05_simultaneous_animations/android/app/src/main/kotlin/com/example/ex10_05_simultaneous_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex10_05_simultaneous_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Develop/ex10_05_simultaneous_animations/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const LogoApp());
+
+class LogoWitget extends StatelessWidget {
+  const LogoWitget({super.key});
+
+  // Leave out the height and width so it fills the animating parent
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: const FlutterLogo(),
+    );
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  const GrowTransition(
+      {required this.child, required this.animation, super.key});
+
+  final Widget child;
+  final Animation<double> animation;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return SizedBox(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  const LogoApp({super.key});
+
+  @override
+  State<LogoApp> createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      })
+      ..addStatusListener((status) => print('$status'));
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GrowTransition(
+      animation: animation,
+      child: const LogoWitget(),
+    );
+  }
+
+  @override
+  void dispose() {
+    // dispose: State 객체를 영구적으로 없앨 때 호출
+    controller.dispose();
+    super.dispose();
+  }
+}
