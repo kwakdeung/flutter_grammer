49b567069729348e1936df734b9ce4c86e08735f
kwakdeung <51012589a@gmail.com>
2022-07-18 10:04:48 +0900

    ex06_01_bringing_it_all_together

--- Develop/ex06_01_bringing_it_all_together/test/widget_test.dart
+++ Develop/ex06_01_bringing_it_all_together/test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ex06_01_bringing_it_all_together/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
