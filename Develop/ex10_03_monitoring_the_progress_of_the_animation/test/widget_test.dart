d2402845e1bb49b6165b1d2072740db95815e5ac
kwakdeung <51012589a@gmail.com>
2022-07-18 10:06:20 +0900

    ex10_03_monitoring_the_progress_of_the_animation

--- Develop/ex10_03_monitoring_the_progress_of_the_animation/test/widget_test.dart
+++ Develop/ex10_03_monitoring_the_progress_of_the_animation/test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ex10_03_monitoring_the_progress_of_the_animation/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
