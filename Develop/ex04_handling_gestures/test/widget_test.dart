7a99d484b8712320ae61018198eac213f47de0b3
kwakdeung <51012589a@gmail.com>
2022-07-18 10:03:59 +0900

    ex04_handling_gestures

--- Develop/ex04_handling_gestures/test/widget_test.dart
+++ Develop/ex04_handling_gestures/test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ex04_handling_gestures/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
