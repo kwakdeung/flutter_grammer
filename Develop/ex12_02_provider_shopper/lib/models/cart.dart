cda2c3843094c134ea4810f4234794d831b16717
kwakdeung <51012589a@gmail.com>
2022-07-20 15:37:22 +0900

    models 추가 - cart.dart, catalog.dart

---
+++ Develop/ex12_02_provider_shopper/lib/models/cart.dart
@@ -0,0 +1,30 @@
+import 'package:ex12_02_provider_shopper/models/catalog.dart';
+import 'package:flutter/foundation.dart';
+
+class CartModel extends ChangeNotifier {
+  late CatalogModel _catalog;
+
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+    notifyListeners();
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  int get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+    notifyListeners();
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+    notifyListeners();
+  }
+}
