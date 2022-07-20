8b2f54a22162be0216254ba2c9de13a63045f09b
kwakdeung <51012589a@gmail.com>
2022-07-20 16:55:26 +0900

    ex12_02_provider_shopper 최종

--- Develop/ex12_02_provider_shopper/lib/main.dart
+++ Develop/ex12_02_provider_shopper/lib/main.dart
@@ -1,10 +1,38 @@
-import 'package:ex12_02_provider_shopper/screens/login.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:ex12_02_provider_shopper/models/cart.dart';
+import 'package:ex12_02_provider_shopper/models/catalog.dart';
+import 'package:ex12_02_provider_shopper/screens/cart.dart';
+import 'package:ex12_02_provider_shopper/screens/catalog.dart';
+import 'package:ex12_02_provider_shopper/screens/login.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
 import 'common/theme.dart';
 
 void main() {
+  setupWindow();
   runApp(const MyApp());
+}
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Demo');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,13 +40,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Provider Demo',
-      theme: appTheme,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const MyLogin(),
-      },
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => CatalogModel()),
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const MyLogin(),
+          '/catalog': (context) => const MyCatalog(),
+          '/cart': (context) => const MyCart(),
+        },
+      ),
     );
   }
 }
---
+++ Develop/ex12_02_provider_shopper/lib/screens/catalog.dart
@@ -0,0 +1,112 @@
+import 'package:ex12_02_provider_shopper/models/cart.dart';
+import 'package:ex12_02_provider_shopper/models/catalog.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyCatalog extends StatelessWidget {
+  const MyCatalog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _MyAppBar(),
+          const SliverToBoxAdapter(child: SizedBox(height: 12)),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => _MyListItem(index)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  final Item item;
+
+  const _AddButton({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    var isInCart = context.select<CartModel, bool>(
+      // .select - Provider
+      (cart) => cart.items.contains(item),
+    );
+
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          : () {
+              var cart = context.read<CartModel>(); // .read - Provider
+              cart.add(item);
+            },
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return Theme.of(context).primaryColor;
+          }
+          return null;
+        }),
+      ),
+      child: isInCart
+          ? const Icon(Icons.check, semanticLabel: 'ADDED')
+          : const Text('ADD'),
+    );
+  }
+}
+
+class _MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      title: Text('Catalog', style: Theme.of(context).textTheme.headline1),
+      centerTitle: true,
+      floating: true,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.shopping_cart),
+          onPressed: () => Navigator.pushNamed(context, '/cart'),
+        ),
+      ],
+    );
+  }
+}
+
+class _MyListItem extends StatelessWidget {
+  final int index;
+
+  const _MyListItem(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    var item = context.select<CatalogModel, Item>(
+      (catalog) => catalog.getByPosition(index),
+    );
+    var textTheme = Theme.of(context).textTheme.headline6;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: LimitedBox(
+        maxHeight: 48,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                color: item.color,
+              ),
+            ),
+            const SizedBox(width: 24),
+            Expanded(
+              child: Text(item.name, style: textTheme),
+            ),
+            const SizedBox(width: 24),
+            _AddButton(item: item),
+          ],
+        ),
+      ),
+    );
+  }
+}
