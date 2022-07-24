419a85a0cb2112c781106f25045edf04e2feadda
kwakdeung <51012589a@gmail.com>
2022-07-24 20:27:49 +0900

    ex01_print

---
+++ Dart/ex01_print/android/app/src/main/kotlin/com/example/ex01_print/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex01_print
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Dart/ex01_print/lib/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('Hello');
+  print('Hello');
+
+  int num = 1;
+  print(num);
+  print('num의 값은 $num');
+
+  print('1 + 2 = ${1 + 2}');
+}
